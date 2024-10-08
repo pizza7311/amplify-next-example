a3c05fcaef1d639017b9ff466e23d5fda131fe69
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2024-10-08 12:23:09 +0900

    fix: next SSG

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,10 @@
-export const revalidate = 10000;
+export const revalidate = 60;
 
 import dayjs from "dayjs";
+
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 const Detail = async ({ params }: { params: { id: string } }) => {
   const data = await fetch(
