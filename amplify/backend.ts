38972991518e972b4dd0f9d766ce58d3b6911589
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2024-10-10 00:20:02 +0900

    dir: move test folder

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { test } from "./functions/test/resource";
+import { test } from "./test/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
